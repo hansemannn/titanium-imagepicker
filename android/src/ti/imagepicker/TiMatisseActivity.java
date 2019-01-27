06d9b3977602d5cf3fa38a5ba4fa6ff4d56cefdf
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-01-27 18:05:17 +0100

    chore: use „Matisse“ library for Android

---
+++ android/src/ti/imagepicker/TiMatisseActivity.java
@@ -0,0 +1,42 @@
+package ti.imagepicker;
+
+import org.appcelerator.kroll.common.Log;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.PicassoEngine;
+
+public class TiMatisseActivity extends Activity {
+	
+	public static final String PROPERTY_MAX_IMAGE_SELECTION = "maxImageSelection";
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        Intent i = getIntent();
+        Bundle extras = i.getExtras();
+        
+        int maxImageSelection = extras.getInt(PROPERTY_MAX_IMAGE_SELECTION);
+        
+        Matisse.from(this)
+            .choose(MimeType.ofImage())
+            .countable(true)
+            .maxSelectable(maxImageSelection)
+            .imageEngine(new PicassoEngine())
+            .forResult(1337);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Just pass on this result upstream
+        setResult(resultCode, data);
+        finish();
+    }
+}
