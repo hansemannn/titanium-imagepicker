93b170a056043e9befc51bc393a5f917e29d4d1c
Prashant Saini <prashant10103497@gmail.com>
2020-01-12 21:51:33 +0530

    feat: refactored the module by adding „resultAsBlob” paramter to either get the absolute file-paths or the „TiBlob” instances

---
+++ android/src/ti/imagepicker/Defaults.java
@@ -0,0 +1,13 @@
+package ti.imagepicker;
+
+public class Defaults {
+	final static String PROPERTY_MAX_IMAGE_SELECTION = "maxImageSelection";
+	final static String PROPERTY_RESULT_AS_BLOB = "resultAsBlob";
+	
+	final static String CALLBACK_PROPERTY_SUCCESS = "success";
+	final static String CALLBACK_PROPERTY_CANCEL = "cancel";
+	final static String CALLBACK_PROPERTY_IMAGES = "images";
+	
+	final static int VALUE_MAX_IMAGE_SELECTION = 3;
+	final static boolean VALUE_RESULT_AS_BLOB = false;
+}
